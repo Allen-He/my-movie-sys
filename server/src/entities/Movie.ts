29da5de66ed91ba58804fc0c377c931887e78375
Allen-He <2426567439@qq.com>
2022-01-31 15:53:09 +0800

    feat: 定义电影实体类（class Movie）；处理对应plainObject的转换问题

---
+++ server/src/entities/Movie.ts
@@ -0,0 +1,44 @@
+import { Type } from "class-transformer";
+import { ArrayMinSize, IsArray, isArray, IsNotEmpty, isNotEmpty, Max, Min } from "class-validator";
+
+export class Movie {
+  @IsNotEmpty({message: '电影名称不能为空'})
+  @Type(() => String) //利用装饰器，使“类型约束”在运行时生效
+  public name: string;
+
+  @IsNotEmpty({message: '电影类型不能为空'})
+  @ArrayMinSize(1, {message: '电影类型至少有一个'})
+  @IsArray({message: '电影类型必须为数组'})
+  @Type(() => String) //利用装饰器，使“类型约束”在运行时生效(class-transformer库建议：如果是一个数组，则直接返回数组的每一项的类型即可。然后配合上述class-validator提供的装饰器@IsArray进行使用)
+  public types: string[];
+
+  @IsNotEmpty({message: '上映地区不能为空'})
+  @ArrayMinSize(1, {message: '上映地区至少有一个'})
+  @IsArray({message: '上映地区必须为数组'})
+  @Type(() => String)
+  public areas: string[];
+
+  @IsNotEmpty({message: '电影时长不能为空'})
+  @Min(30, {message: '电影时长最短为30分钟'})
+  @Max(300, {message: '电影时长最长为300分钟'})
+  @Type(() => Number)
+  public duration: number;
+
+  @IsNotEmpty({message: '“是否即将上映”不能为空'})
+  @Type(() => Boolean)
+  public isComing: boolean = false;
+
+  @IsNotEmpty({message: '“是否热映”不能为空'})
+  @Type(() => Boolean)
+  public isHot: boolean = false;
+
+  @IsNotEmpty({message: '“是否为经典”不能为空'})
+  @Type(() => Boolean)
+  public isClassic: boolean = false;
+
+  @Type(() => String)
+  public desc?: string;
+
+  @Type(() => String)
+  public poster?: string;
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,28 @@
-let a: string = 'sdfaf';
-console.log(a);
-console.log('sdfs');
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
+import { Movie } from "./entities/Movie";
+
+// const m = new Movie();
+// m.name = '只有芸知道';
+// m.types = ['爱情'];
+// m.areas = ['成都'];
+// m.duration = 100;
+
+// validate(m).then(res => {
+//   console.log(res);
+// })
+
+const m: any = {
+  name: 123,
+  types: ['爱情'],
+  areas: ['成都'],
+  duration: 100,
+  isHot: true
+};
+
+const mObj = plainToClass(Movie, m);
+
+validate(mObj).then(res => {
+  console.log(res);
+})
